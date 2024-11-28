320f21f81176cf6cde913ccd3a762989818c97d7
evan <evanhyde1@gmail.com>
2024-11-28 11:09:13 -0330

    GAME WORK AGAIN

--- BlackJack.py
+++ BlackJack.py
@@ -1,71 +1,86 @@
 #Procedural Programming Course Project - BlackJack
 import random
-def main():
-   while True:
-      deck = init_deck()
-      hand = [[], []] # one is player, two is dealer
-      #initial deal of two cards to player and dealer
-      for start_deal in range(2):
-         hand[0].append(deal_a_card(deck))
-         hand[1].append(deal_a_card(deck))
+def main():      
+ while True:
 
-      #start the actual part of the game
-      player_turn(hand,deck) #Ask player to h/s
-      dealers_turn(hand, deck)#Dealer hits till 17 or bust
-
-      players_hand_score = hand_score(hand[0])
-      dealer_hand_score = hand_score(hand[1])
-      print(f"Your points {players_hand_score}")
-      print(f"Dealers points {dealer_hand_score}")
-      win_game(players_hand_score, dealer_hand_score)
-      
-      restart_game = input("Play again? (y/n): ")
-      if restart_game.lower() in ["yes", "y"]:
-         continue
-      elif restart_game.lower() in ["no", "n"]:
-         print("\nCome back soon!")
-         print("Bye!")
-         break
-      else:
-         print("Please enter 'y' or 'n'")
+    deck = init_deck()
+    hand = [[], []] # one is player, two is dealer 
+    for start_deal in range(2): #initial deal of two cards to player and dealer
+            hand[0].append(deal_a_card(deck))
+            hand[1].append(deal_a_card(deck))
+            
+    print("\nDEALERS SHOW CARDs:")
+    print(f"{hand[1][0][0]} of {hand[1][0][1]}")
+    player_turn(hand,deck) 
+    dealers_turn(hand, deck)
+    players_hand_score = hand_score(hand[0])
+    dealer_hand_score = hand_score(hand[1])
+    show_dealer_cards(hand)
+    print(f"\nYour points: {players_hand_score}")
+    print(f"\nDealers points: {dealer_hand_score}")
+    win_game(players_hand_score, dealer_hand_score)
+    ###############################################
+   
+    restart_game = input("Play again? (y/n): ")
+    if restart_game.lower() in ["yes", "y"]:
+        continue
+    elif restart_game.lower() in ["no", "n"]:
+        print("\nCome back soon!")
+        print("Bye!")
+        break
+    else:
+        print("Please enter 'y' or 'n'")
       
 
 def show_dealer_cards(hand):
       print("\nDEALERS CARDS:")
-      card_format(hand[1])
+      print(hand[1])
 
 def show_players_cards(hand):
      print("\nYOUR CARDS:")
-     card_format(hand[0])
+     print(hand[0])
        
 
-def card_format(cards):
-   for card in cards:
-      print(f"{card[0]} of {card[1]}")
-   
+
+  
 def player_turn(hand, deck):
-   while True:
-       players_hand_score = hand_score(hand[0])
-       if players_hand_score > 21:
-          print("BUSTED!!! Dealer wins!")
-          break
-       player_turn_choice = input("Hit or Stand (H/S)")
-       if player_turn_choice.lower() in ["hit", "h"]:
-         hand[0].append(deal_a_card(deck))
-       elif player_turn_choice.lower() in ["Stand","s"]:
+ while True:
+     players_hand_score = hand_score(hand[0])
+     show_players_cards(hand)
+     print(f"\nYour points: {players_hand_score}")
+     if players_hand_score > 21:
+         print("BUSTED, Dealer wins!")
          break
-       else:
-           print("Please choose hit or stand")
+        
+     player_turn = input("Hit or Stand (H/S: )")
+     if player_turn.lower() in ["hit", "h"]:
+         hand[0].append(deal_a_card(deck))
+         
+         
+     elif player_turn.lower() in ["stand", "s"]:
+         break
+     else:
+         print("Please choose hit or stand")
+
+   
+       
 
 def dealers_turn(hand, deck):
-   dealers_hand_score = hand_score(hand[1])
-   while dealers_hand_score < 17:
-      print("greater than 17")
-      hand[1].append(deal_a_card(deck))
-      dealers_hand_score = hand_score(hand[1])
-   if dealers_hand_score > 21:
-      print ("You WIN, Dealer has busted.")
-   
+    while True:
+        dealers_hand_score = hand_score(hand[1])
+        print("\nDEALERS TURN:")
+        show_dealer_cards(hand)
+
+        if dealers_hand_score < 17:
+            print("DEALER HITS!")
+            hand[1].append(deal_a_card(deck))    
+        elif dealers_hand_score > 21:
+                print("DEALER BUSTED YOU WIN")
+                break
+        else:
+            print("DEALER STANDS.")
+            break
+
 
 #deal a random card
 def deal_a_card(deck):
@@ -77,10 +92,10 @@ def deal_a_card(deck):
 def hand_score(hand):
     score = 0
     aces = 0
-    for cards in hand:
-      score += cards[2]
-      if cards[0] in ["A"]:
-          aces += 1
+    for card in hand:
+     score += card[2]
+     if card[0] in ["A"]:
+        aces += 1
     while score > 21 and aces > 0:
         score -=10 
         aces -= 1
