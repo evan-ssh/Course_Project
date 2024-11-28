789d1ca5f215785f4dd03877ad09effe4b5945bb
evan <evanhyde1@gmail.com>
2024-11-28 17:33:29 -0330

    submit project

--- BlackJack.py
+++ BlackJack.py
@@ -1,80 +1,65 @@
 #Procedural Programming Course Project - BlackJack
 import random
-#import playerbank.py as playerbank 
+from db import load_bank, save_bank 
 
 
 
 def main():   
- FILENAME = "playermoney.txt"
- player_money = load_bank(FILENAME)  
- while True:
-  
-        print("BLACKJACK!")
-        print("Blackjack Payout is 3:2")
-        print(f"PLAYER MONEY: {player_money}")
+    FILENAME = "playermoney.txt"
+    player_money = load_bank(FILENAME)  
+    if player_money < 5:
+     print("\nYour balance is too low. Starting with $100.")
+     player_money = 100.0
+     save_bank(FILENAME, player_money)
 
+
+
+    while True:
+        print("\nBLACKJACK!")
+        print("\nBlackjack Payout is 3:2")
+        print(f"\nPLAYER MONEY: {player_money}")
+        
+        player_bet = place_bet(player_money)   # get player bet 
        
-            
-    #############
-    deck = init_deck()
-    hand = [[], []] # one is player, two is dealer 
-    #############
-    for start_deal in range(2): #initial deal of two cards to player and dealer
-                hand[0].append(deal_a_card(deck))
-                hand[1].append(deal_a_card(deck))
-`
+        
+        deck = init_deck() #load cards
+        hand = [[], []] # one is player, two is dealer 
+     
+        for start_deal in range(2): #initial deal of two cards to player and dealer
+                    hand[0].append(deal_a_card(deck))
+                    hand[1].append(deal_a_card(deck))
 
-    ##########################################
-    print("\nDEALERS SHOW CARDs:")
-    print(f"{hand[1][0][0]} of {hand[1][0][1]}")
-    player_turn(hand,deck) 
-    dealers_turn(hand, deck)
-    players_hand_score = hand_score(hand[0])
-    dealer_hand_score = hand_score(hand[1])
-    show_dealer_cards(hand)
-    print(f"\nYour points: {players_hand_score}")
-    print(f"\nDealers points: {dealer_hand_score}")
-    player_money_won = win_game(players_hand_score, dealer_hand_score, player_money)
-    save_bank(FILENAME, player_money_won)
-###############################################
 
-        restart_game = input("Play again? (y/n): ")
-        if restart_game.lower() in ["yes", "y"]:
-            continue
-        elif restart_game.lower() in ["no", "n"]:
-            print("\nCome back soon!")
-            print("Bye!")
-            break
+        ##########################################
+        print("\nDEALERS SHOW CARDs:")
+        print(f"{hand[1][0][0]} of {hand[1][0][1]}") # BlackJack logic #
+        player_turn(hand,deck) 
+        dealers_turn(hand, deck)
+        players_hand_score = hand_score(hand[0])
+        dealer_hand_score = hand_score(hand[1])
+        show_dealer_cards(hand)
+        print(f"\nYour points: {players_hand_score}")
+        print(f"\nDealers points: {dealer_hand_score}")
+        player_money_won = win_game(players_hand_score, dealer_hand_score, player_money, player_bet)
+        save_bank(FILENAME, player_money_won)
+    ###############################################
+
+        restart_game = input("Play again? (y/n): ") # play again #
+        if restart_game.lower() == 'y' or 'yes':
+            print(f"Starting next game")
         else:
-            print("Please enter 'y' or 'n'")
+            print(f"Goodbye! thanks for playing.")
+            break
         
-        #############################################
+    ###############################################
     
 
-
-        
-
-
-def load_bank(FILENAME):
-    try:
-        with open(FILENAME, "r") as file:
-            playermoney = float(file.read().strip())
-            print(f"Your money: {playermoney}")
-    except FileNotFoundError:
-        playermoney = 100
-        print("NO BANK FOUND STARTING WITH 100")
-        return 100.0
-  
-    
-def save_bank(FILENAME):
-    with open(FILENAME, "w") as file:
-        file.write(str(amount))
 def place_bet(player_money):
     while True:
         try:
             player_bet = float(input("BET AMOUNT(5-1000):")) 
-            if player_bet = 0 
-             return 
+            if player_bet == 0: 
+               return 0
             if player_bet < 5:
                 print("Please enter a higher amount")        
             elif player_bet > 1000:
@@ -84,16 +69,16 @@ def place_bet(player_money):
             else:
                 return player_bet
         except ValueError:
-                print("Please enter a number(0 to exit)")
+                print("Please enter a number")
 def show_dealer_cards(hand):
     print("\nDEALERS CARDS:")
     for card in hand[1]:
-     print(f"{card[0]} of {card[1]}")
+     print(f"{card[0]} of {card[1]}\n")
 
 def show_players_cards(hand):
     print("\nYOUR CARDS:")
     for card in hand[0]:
-     print(f"{card[0]} of {card[1]}")
+     print(f"{card[0]} of {card[1]}\n")
  
 
 
@@ -139,14 +124,14 @@ def dealers_turn(hand, deck):
             break
 
 
-#deal a random card
-def deal_a_card(deck):
+
+def deal_a_card(deck): #deal a random card
    card = random.choice(deck)
    deck.remove(card)
    return card
 
-#Calculate Total Score Of a Given Hand
-def hand_score(hand):
+
+def hand_score(hand): #Calculate Total Score Of a Given Hand
     score = sum(card[2] for card in hand)
     aces = sum(1 for card in hand if card[0] == "A")
     while score > 21 and aces > 0:
@@ -156,8 +141,8 @@ def hand_score(hand):
         
 
         
-#initialize our deck
-def init_deck():
+ 
+def init_deck(): #initialize our deck
     deck = []
     suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
     ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J"]
@@ -176,23 +161,31 @@ def init_deck():
    
 
 
-
-def win_game(player_hand_score, dealer_hand_score,playermoney):
-    if player_hand_score > 21:
+#determine game winner
+def win_game(player_hand_score, dealer_hand_score, player_money, player_bet):
+    if player_hand_score == 21: #if player gets blackjack 
+     print("BLACKJACK!!")
+     player_money += player_bet * 1.5
+     return player_money
+    elif player_hand_score > 21:
       print("Dealer wins")
-      playermoney += player_bet
+      player_money += player_bet 
+      return player_money
     elif dealer_hand_score > 21:
         print("Player wins")
-        playermoney -= player_bet
+        player_money -= player_bet 
+        return player_money
     elif player_hand_score > dealer_hand_score:
         print("Player Wins")
-        playermoney += player_bet
+        player_money += player_bet 
+        return player_money
     elif dealer_hand_score > player_hand_score:
         print("Dealer wins")
-        playermoney -= player_bet
+        player_money -= player_bet
+        return player_money
     else:
         print("You have tied")
-save_bank()
+    return player_money 
 
 
 
--- db.py
+++ db.py
@@ -1,9 +1,17 @@
+#players blackjack bank
 FILENAME = "playermoney.txt"
-def load_bank():
-    with open(FILENAME, "r") as file:
-        line = file.read().strip()
-        
+def load_bank(FILENAME):    
+    try:
+        with open(FILENAME, "r") as file:
+            return float(file.read().strip())
+    except FileNotFoundError:
+        print("no money in bank. starting with 100")
+        return 100.0        
 
-def save_bank():
-    with open(FILENAME, "w") as file:
-        file.write()_
+def save_bank(FILENAME, amount):
+    try:
+        with open(FILENAME, "w") as file:
+            file.write(str(amount))
+    except FileNotFoundError:
+        print("File cant be found to be saved.")
+   
