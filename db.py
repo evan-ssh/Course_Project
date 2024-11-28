07332d23011346a91b9dbae56420f95759f8f1f8
evan <evanhyde1@gmail.com>
2024-11-28 16:46:39 -0330

    betting system

---
+++ db.py
@@ -0,0 +1,9 @@
+FILENAME = "playermoney.txt"
+def load_bank():
+    with open(FILENAME, "r") as file:
+        line = file.read().strip()
+        
+
+def save_bank():
+    with open(FILENAME, "w") as file:
+        file.write()_
